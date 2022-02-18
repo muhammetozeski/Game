385e35c5287da6338d3d2b7c7920d60c573bf318
Muhammet Özeski <muhammetozeski@gmail.com>
2022-02-18 14:35:37 +0300

    added chain

---
+++ Assets/test folder/chain/ChainConnector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestScripts
+{
+   // [ExecuteInEditMode]
+    public class ChainConnector : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           // GetComponent<HingeJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+
+        }
+    }
+}
